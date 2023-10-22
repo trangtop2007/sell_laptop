4effb3236eade98303ca10486c29b2d5d2d86a35
trang <trangtop2007@gmail.com>
2023-10-22 20:48:24 +0700

    update-3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,6 @@
 <template>
-  Type a number: <input v-model.number="number" />
-  <p>{{ tweened.number.toFixed(0) }}</p>
+  <router-view></router-view>
 </template>
-<script setup>
-import { ref, reactive, watch } from 'vue'
-import gsap from 'gsap'
-
-const number = ref(0)
-const tweened = reactive({
-  number: 0
-})
-
-watch(number, (n) => {
-  gsap.to(tweened, { duration: 0.5, number: Number(n) || 0 })
-})</script>
 <!-- <template>
   <div>
     <button @click="login">Login Using Google</button>
